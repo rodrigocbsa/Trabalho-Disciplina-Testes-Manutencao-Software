ee0608124b87933e31b79e8d87f26725f71bfcbf
Isadora <isabarrosopassos@gmail.com>
2023-09-30 14:25:08 -0300

    Teste de Todos os Nos

--- Trabalho 1 - Testes de Caixa Branca/Biblioteca/test/negocio/CaminhosBasicosTest.java
+++ Trabalho 1 - Testes de Caixa Branca/Biblioteca/test/negocio/CaminhosBasicosTest.java
@@ -7,32 +7,9 @@ import org.junit.jupiter.api.Test;
 class CaminhosBasicosTest {
 
 	@Test
-	void caminho1() {
+	void calcularNumeroDiasEmprestimo_caminhosBasico_caminho1() {
 		fail("Not yet implemented");
 	}
 	
-	@Test
-	void caminho3() {
-		fail("Not yet implemented");
-	}
 	
-	@Test
-	void caminho4() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	void caminho5() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	void caminho6() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	void caminho7() {
-		fail("Not yet implemented");
-	}
 }
